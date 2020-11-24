e023d60591341020a301a7e3cf279bf23f185405
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2020-11-24 23:53:14 +0500

    Revert "Fixed some bugs in user validation"
    This reverts commit 775eed4c

--- src/main/java/com/ssp/platform/PlatformApplication.java
+++ src/main/java/com/ssp/platform/PlatformApplication.java
@@ -2,8 +2,6 @@ package com.ssp.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
 public class PlatformApplication {
--- src/main/java/com/ssp/platform/controller/UserController.java
+++ src/main/java/com/ssp/platform/controller/UserController.java
@@ -58,7 +58,6 @@ public class UserController
     }
 
     @GetMapping("/login")
-    @CrossOrigin("https://ssp-platform.herokuapp.com/")
     public ResponseEntity<?> authenticateUser(@RequestHeader("username") String username, @RequestHeader("password") String password)
     {
         ValidatorResponse validatorResponse;
