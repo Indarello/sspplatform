4201cafbb528478bbb7786a559e22c0a39f27722
Indarello <46714497+Indarello@users.noreply.github.com>
2020-12-05 01:58:40 +0700

    Добавлена функция ограничения по ограничения типа загруженного файла

--- src/main/java/com/ssp/platform/property/FileProperty.java
+++ src/main/java/com/ssp/platform/property/FileProperty.java
@@ -1,12 +1,15 @@
 package com.ssp.platform.property;
 
+import com.ssp.platform.response.ValidatorResponse;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Component
 @Data
 @ConfigurationProperties(prefix = "file")
 public class FileProperty {
     private String uploadDirectory;
+    private String[] restrictedTypes;
 }
--- src/main/java/com/ssp/platform/validate/FileValidator.java
+++ src/main/java/com/ssp/platform/validate/FileValidator.java
@@ -1,9 +1,13 @@
 package com.ssp.platform.validate;
 
+import com.ssp.platform.property.FileProperty;
 import com.ssp.platform.response.ValidatorResponse;
 import com.ssp.platform.validate.ValidatorMessages.FileValidatorMessages;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class FileValidator extends Validator{
@@ -14,6 +18,29 @@ public class FileValidator extends Validator{
 
     private final int MAX_FILENAME_SIZE = 255;
 
+    private final String[] restrictedTypes;
+
+    private final Pattern restrictedRegex;
+
+    @Autowired
+    public FileValidator(FileProperty fileProperty)
+    {
+        restrictedTypes = fileProperty.getRestrictedTypes();
+        String regex = ".*(\\.";
+
+        for (String s : restrictedTypes)
+        {
+            regex = regex.concat(s);
+            regex = regex.concat("|\\.");
+        }
+        regex = regex.substring(0, regex.length() - 3);
+        regex = regex.concat(")");
+        //System.out.println(regex);
+        restrictedRegex = Pattern.compile(regex);
+
+
+    }
+
     public ValidatorResponse validateFile(MultipartFile file){
         if (file == null){
             return new ValidatorResponse(true, FileValidatorMessages.OK);
@@ -23,15 +50,29 @@ public class FileValidator extends Validator{
             return new ValidatorResponse(false, FIELD_NAME, FileValidatorMessages.WRONG_FILE_SIZE_ERROR);
         }
 
-        if (file.getOriginalFilename().length() > MAX_FILENAME_SIZE){
+        String fileName = file.getOriginalFilename();
+
+        if (fileName.length() > MAX_FILENAME_SIZE){
             return new ValidatorResponse(false, FIELD_NAME, FileValidatorMessages.WRONG_FILENAME_SIZE_ERROR);
         }
 
-        /**if (!isMatch(file.getOriginalFilename(), "([а-яА-Яa-zA-Z0-9\\s_\\\\.\\-\\(\\):])+(.rar|.zip)$")){
+        /**if (!isMatch(fileName, "([а-яА-Яa-zA-Z0-9\\s_\\\\.\\-\\(\\):])+(.rar|.zip)$")){
             return new ValidatorResponse(false, FIELD_NAME, ValidatorMessages.WRONG_FILE_TYPE_ERROR);
         }*/
 
+        if(checkRestrictedType(fileName))
+        {
+            return new ValidatorResponse(false, FIELD_NAME, "Файл с таким расширением запрещено загружать");
+        }
+
         return new ValidatorResponse(true, FileValidatorMessages.OK);
+    }
+
+    private boolean checkRestrictedType(String fileName)
+    {
+        Matcher matcher = restrictedRegex.matcher(fileName);
+
+        return matcher.matches();
     }
 
     public ValidatorResponse validateFiles(MultipartFile[] files){
