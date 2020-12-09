41c5b7749d600138e43b64306c83835f8ea48450
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2020-12-09 12:09:10 +0500

    Правка сообщения об ошибке

--- src/main/java/com/ssp/platform/service/impl/SupplyServiceImpl.java
+++ src/main/java/com/ssp/platform/service/impl/SupplyServiceImpl.java
@@ -118,6 +118,8 @@ public class SupplyServiceImpl implements SupplyService {
                     }
                 }
 
+                supplyEntity.setFiles(fileEntities);
+
                 break;
 
             case "employee":
