056deb69e0aaeb36e3c87d8dd7a81e7688b3a566
Indarello <46714497+Indarello@users.noreply.github.com>
2020-12-16 23:42:39 +0700

    Small fix

--- src/main/java/com/ssp/platform/controller/PurchaseController.java
+++ src/main/java/com/ssp/platform/controller/PurchaseController.java
@@ -26,11 +26,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
