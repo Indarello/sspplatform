4dbba6c01231f03d357e7062efd57e0cf3468707
Gypst <mrdimas27@gmail.com>
2020-12-16 10:35:39 +0700

    Настройки логера по умолчанию загружаются нужные (теперь без JVM options). Логирование происходит по датам, бэкап каждый день (00:00)

--- src/main/java/com/ssp/platform/PlatformApplication.java
+++ src/main/java/com/ssp/platform/PlatformApplication.java
@@ -1,49 +1,26 @@
 package com.ssp.platform;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.apache.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-import org.apache.log4j.PropertyConfigurator;
-
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class PlatformApplication {
 
-//	private static java.util.logging.Logger logger;
 	private static final Logger logger = Logger.getLogger(PlatformApplication.class);
 
 
 	public static void main(String[] args) throws IOException {
-		//Попытки загрущить настройки для JUL
-//		System.out.println(System.getProperty("java.util.logging.config.file"));
-//		System.setProperty("java.util.logging.config.file",
-//				".\\src\\main\\java\\resorces\\log.properties");
 
-		//Успешная загрузка настроек JUL
-//		String path = PlatformApplication.class.getClassLoader()
-//												.getResource("log.properties")
-//												.getFile();
-//		System.setProperty("java.util.logging.config.file", path);
-//		logger = java.util.logging.Logger.getLogger(PlatformApplication.class.getName());
-
-
-		//Успешное решение, но без даты
-//		FileHandler fh = new FileHandler("./log/log.log", true);
-//		fh.setFormatter(new SimpleFormatter());
-//		fh.setLevel(Level.ALL);
-//		logger.addHandler(fh);
-
-
-
-		PropertyConfigurator.configure("log4j.properties");
-		logger.debug("Sample debug message");
-		logger.info("Sample info message");
-		logger.warn("Sample warn message");
-		logger.error("Sample error message");
-		logger.fatal("Sample fatal message");
+		//Загрузка настроек логера из log4j.properties
+		Properties props = new Properties();
+		props.load(new FileInputStream("./src/main/resources/log4j.properties"));
+		PropertyConfigurator.configure(props);
 
 		logger.info("Starting application..");
 
