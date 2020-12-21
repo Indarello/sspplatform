bc4ec2c842967d439f4129fad53941a073ff16c8
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2020-12-21 12:01:34 +0500

    Подключил логгер для отладки ошибки

--- src/main/java/com/ssp/platform/controller/UserController.java
+++ src/main/java/com/ssp/platform/controller/UserController.java
@@ -300,6 +300,14 @@ public class UserController
                     validUser.getEmail(),
                     validUser.getStatus()};
 
+            if (oldUser.getStatus().equals("NotApproved")){
+                logger.info("условие 1");
+            }
+
+            if (validUser.getStatus().equals("Approved")){
+                logger.info("условие 2");
+            }
+
             if (oldUser.getStatus().equals("NotApproved") && validUser.getStatus().equals("Approved")){
                 logger.info("status");
                 sspPlatformBot.notifyAboutStatusChanges(validUser);
